b9b222c80f063008fb416e7e0bf365308ecaed6b
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2023-01-31 21:01:07 +0530

    commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  SERV_PORT: 8989,
+  DB_KEY:
+    "mongodb+srv://het:Hiiiii@dbx.caggcrd.mongodb.net/userdata?retryWrites=true&w=majority",
+  _LINK: "http://localhost:8989",
+};
